d268a1580e7073f379a9c08c88590321094dd0eb
Milorad Bozic <miloradbozic29@gmail.com>
2018-10-17 13:57:23 +0200

    voa qti test:

--- app/phone-list.component.js
+++ app/phone-list.component.js
@@ -1,28 +1,23 @@
 'use strict';
-
 // Register `phoneList` component, along with its associated controller and template
 angular.
   module('phonecatApp').
   component('phoneList', {
     template:
-        '<ul>' +
-          '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
+        '{{question}} <ul>' +
+          '<li ng-repeat="choice in choices">' +
+            '<span>{{choice}}</span>' +
           '</li>' +
         '</ul>',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
+    controller: function PhoneListController($http, $scope) {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/api/runner/start'
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            $scope.question = response.data.question.questionText;
+            $scope.choices =  response.data.question.choices;
+      }, function errorCallback(response) {
+      });
     }
   });

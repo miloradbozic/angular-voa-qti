259370b583271e9fb45451b38e9bfa11114a1bbb
Milorad Bozic <miloradbozic29@gmail.com>
2018-10-19 15:23:48 +0200

    keycloak + additional

--- app/app.js
+++ app/app.js
@@ -1,4 +1,21 @@
 'use strict';
+/*
+var keycloakConfig = {
+    "url": "http://localhost:8080/auth",
+    "realm": "Demo",
+    "clientId": "voa-qti-front",
+//    "credentials": {
+//        "secret": "1a1b98b6-66c5-4384-a4b4-7361717e773e"
+//    }
+};
+var keycloak = Keycloak(keycloakConfig);
+keycloak.init({
+    onLoad: 'login-required'
+}).success(function () {
+    //User has logged in
+    console.log("Keyclaok logged in");
+});
+*/
 
 // Define the `phonecatApp` module
 angular.module('phonecatApp', []);
--- app/phone-list.component.js
+++ app/phone-list.component.js
@@ -6,18 +6,38 @@ angular.
     template:
         '{{question}} <ul>' +
           '<li ng-repeat="choice in choices">' +
-            '<span>{{choice}}</span>' +
+            '<input type="checkbox" value="choice"><span>{{choice}}</span>' +
           '</li>' +
-        '</ul>',
+        '</ul>' +
+        '<a href="" ng-click="$ctrl.next()">Next</a>',
     controller: function PhoneListController($http, $scope) {
+
+    //init
         $http({
             method: 'GET',
-            url: 'http://localhost:8080/api/runner/start'
+            url: 'https://localhost:9090/api/runner/start'
+        }).then(function successCallback(response) {
+            console.log(response);
+            console.log(response.data);
+            $scope.question = response.data.question.questionText;
+            $scope.choices =  response.data.question.choices;
+          }, function errorCallback(response) {
+        });
+
+      this.next = function() {
+        $http({
+            method: 'GET',
+            url: 'https://localhost:9090/api/runner/next'
         }).then(function successCallback(response) {
             console.log(response.data);
             $scope.question = response.data.question.questionText;
             $scope.choices =  response.data.question.choices;
-      }, function errorCallback(response) {
-      });
+          }, function errorCallback(response) {
+        });
+      };
+
+//      function previous() {
+//        console.log("Halo prev");
+//      }
     }
   });
